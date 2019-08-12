3be88213f10d060efed94b59509df50831c0354b
Mia <mia@Apples-MacBook-Pro-30.local>
2019-08-12 14:57:41 -0400

    table practice

--- emojional/emojional/ViewController.swift
+++ emojional/emojional/ViewController.swift
@@ -11,16 +11,24 @@ import UIKit
 class ViewController: UIViewController {
  
     let emojis = ["😡" : "angry face" , "😬" : "nervous face"]
+    let customMessages = [
+        "angry face" : ["Calm down" , "Take a deep breath" , "It is not that big of a deal"],
+        "nervous face" : ["Do not worry" , "It is okay" , "Calm down, it will be okay"],
+        
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     @IBAction func showMessage(sender: UIButton){
         
         if let selectedEmotion = sender.titleLabel?.text{
+            let number = Int.random(in: 0 ... 2)
             let titleText = "Welcome to my App"
-            let messageText = emojis[selectedEmotion]
+            let messageText = emojis[selectedEmotion]!
             
-            let alertController = UIAlertController(title: "\(titleText)", message: "\(messageText)", preferredStyle: UIAlertController.Style.alert)
+            let emojiMessage = customMessages[emojis[selectedEmotion]!]?[number]
+
+            let alertController = UIAlertController(title: "\(titleText)", message: "\(emojiMessage!)", preferredStyle: UIAlertController.Style.alert)
             
             alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
             present(alertController, animated: true, completion: nil)
